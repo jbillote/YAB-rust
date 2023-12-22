2c19728b8399e79a32ec54bdabffebd1bf5b0979
jbillote <jbillote@gmail.com>
2023-12-22 12:13:27 -0800

    Add Twitter embed functionality

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,41 @@
+use regex::Regex;
+use serde::Deserialize;
 use serenity::{
     all::{
         Context, CreateInteractionResponse, CreateInteractionResponseMessage, EventHandler,
         GatewayIntents,
     },
     async_trait,
-    model::{application::Command, application::Interaction, gateway::Ready},
+    builder::{CreateEmbed, CreateMessage},
+    model::{application::Command, application::Interaction, channel::Message, gateway::Ready},
     Client,
 };
-use std::env;
+use std::{env, fs, process::exit};
+use toml;
 
 mod commands;
 mod models;
+mod twitter;
 
 struct Handler;
+
+#[derive(Deserialize)]
+struct Config {
+    discord: Discord,
+    frame_data: FrameData,
+}
+
+#[derive(Deserialize)]
+struct Discord {
+    pub token: String,
+    pub statuses: Vec<String>,
+    pub status_interval: i64,
+}
+
+#[derive(Deserialize)]
+struct FrameData {
+    pub host: String,
+}
 
 #[async_trait]
 impl EventHandler for Handler {
@@ -31,6 +54,18 @@ impl EventHandler for Handler {
         }
     }
 
+    async fn message(&self, ctx: Context, msg: Message) {
+        let split_message = msg.content.split(" ");
+        for m in split_message {
+            let twitter_regex =
+                Regex::new(r"(\bx|\btwitter)\.com\/\w{1,15}\/(status|statuses)\/\d{2,20}").unwrap();
+            if twitter_regex.is_match(m) {
+                let url = twitter_regex.find(m).unwrap();
+                twitter::twitter::generate_twitter_embed(&ctx, &msg, url.as_str()).await;
+            }
+        }
+    }
+
     async fn ready(&self, ctx: Context, ready: Ready) {
         println!("{} is connected!", ready.user.name);
 
@@ -42,9 +77,26 @@ impl EventHandler for Handler {
 
 #[tokio::main]
 async fn main() {
-    let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
+    let args: Vec<String> = env::args().collect();
+    let config_path = &args[1];
+    let config_file = match fs::read_to_string(config_path) {
+        Ok(c) => c,
+        Err(_) => {
+            eprintln!("Could not read file {}", config_path);
+            exit(1);
+        }
+    };
+    let config: Config = match toml::from_str(&config_file) {
+        Ok(c) => c,
+        Err(_) => {
+            eprintln!("Unable to read data from {}", config_path);
+            exit(1);
+        }
+    };
+    let token = config.discord.token;
+    let intents = GatewayIntents::GUILD_MESSAGES | GatewayIntents::MESSAGE_CONTENT;
 
-    let mut client = Client::builder(token, GatewayIntents::empty())
+    let mut client = Client::builder(&token, intents)
         .event_handler(Handler)
         .await
         .expect("Error creating client");
---
+++ src/twitter/twitter.rs
@@ -0,0 +1,100 @@
+use crate::models::vxtwitter::VXTwitter;
+
+use regex::Regex;
+use reqwest::Error;
+use serenity::{
+    builder::{CreateEmbed, CreateEmbedAuthor, CreateEmbedFooter, CreateMessage},
+    client::Context,
+    model::{channel::Embed, channel::Message, timestamp::Timestamp},
+};
+
+pub async fn generate_twitter_embed(ctx: &Context, msg: &Message, url: &str) {
+    let trim_regex = Regex::new(r"\w{1,15}\/(status|statuses)\/\d{2,20}").unwrap();
+    let uri = trim_regex.find(url).unwrap();
+    let info = get_tweet_info(uri.as_str()).await;
+
+    let author = format!(
+        "{user_name} (@{screen_name})",
+        user_name = info.user_name,
+        screen_name = info.screen_name
+    );
+    let author_url = format!(
+        "https://twitter.com/{screen_name}",
+        screen_name = info.screen_name
+    );
+    let split_description: Vec<&str> = info.text.split(" ").collect();
+
+    let mut embeds: Vec<CreateEmbed> = Vec::new();
+    let mut videos: Vec<String> = Vec::new();
+
+    for link in info.media_urls {
+        let split_url: Vec<&str> = link.split(".").collect();
+        let extension = split_url.last().unwrap();
+
+        if extension.contains("jpg") || extension.contains("jpeg") || extension.contains("png") {
+            if embeds.is_empty() {
+                embeds.push(
+                    CreateEmbed::new()
+                        .title("Original Tweet")
+                        .url(&info.tweet_url)
+                        .author(
+                            CreateEmbedAuthor::new(&author)
+                                .icon_url(&info.user_profile_image_url)
+                                .url(&author_url),
+                        )
+                        .description(info.text.replace(split_description.last().unwrap(), ""))
+                        .footer(
+                            CreateEmbedFooter::new("Twitter")
+                                .icon_url("http://i.toukat.moe/twitter_logo.png"),
+                        )
+                        .timestamp(Timestamp::from_unix_timestamp(info.timestamp).unwrap())
+                        .image(link),
+                );
+            } else {
+                embeds.push(CreateEmbed::new().url(&info.tweet_url).image(link));
+            }
+        } else if extension.contains("mp4") {
+            videos.push(link);
+        }
+    }
+
+    if embeds.is_empty() {
+        embeds.push(
+            CreateEmbed::new()
+                .title("Original Tweet")
+                .url(&info.tweet_url)
+                .author(
+                    CreateEmbedAuthor::new(&author)
+                        .icon_url(&info.user_profile_image_url)
+                        .url(&author_url),
+                )
+                .description(info.text.replace(split_description.last().unwrap(), ""))
+                .footer(
+                    CreateEmbedFooter::new("Twitter")
+                        .icon_url("http://i.toukat.moe/twitter_logo.png"),
+                )
+                .timestamp(Timestamp::from_unix_timestamp(info.timestamp).unwrap()),
+        );
+    }
+
+    let builder = CreateMessage::new().embeds(embeds);
+    let res = msg.channel_id.send_message(&ctx.http, builder).await;
+
+    if let Err(why) = res {
+        println!("Error sending message: {why:?}");
+    }
+
+    for link in videos {
+        if let Err(why) = msg.channel_id.say(&ctx.http, link).await {
+            println!("Error sending message: {why:?}");
+        }
+    }
+}
+
+async fn get_tweet_info(path: &str) -> VXTwitter {
+    let url = format!("https://api.vxtwitter.com/{path}", path = path);
+    let response = reqwest::get(&url).await.unwrap();
+    let info: VXTwitter = response.json().await.unwrap();
+
+    return info;
+}
